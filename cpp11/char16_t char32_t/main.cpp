f76e4fb46cd95b33edfb15f1e7877ec4e8ada06b
Unknown <zie35-gymiko@jcom.zaq.ne.jp>
2017-10-12 23:57:06 +0900

    Unicode対応機能テスト用プロジェクトを追加

---
+++ cpp11/char16_t char32_t/main.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <locale>
+#include <codecvt>
+
+int main()
+{
+	std::u16string s16 = u"吉野家";
+	std::u32string s32 = U"吉野家";
+
+	//assert(s16.size() == 4); // サロゲートペアを含むので1文字分多い
+	assert(s32.size() == 3); // UTF-32にサロゲートペアはない
+
+	// WARNING:これより下 visual studioのバグにより正常動作しない
+	// UTF-8とUTF-32の相互変換を行うコンバーター
+	// std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter;
+
+	// UTF-8からUTF-32に変換
+	// std::string u8str = u8"あいうえお";
+	// std::u32string u32str = converter.from_bytes(u8str);
+
+	// コードポイント数を取得
+	// std::size_t codepoint_count = u32str.size();
+	// std::cout << codepoint_count << std::endl;
+
+	return 0;
+}
